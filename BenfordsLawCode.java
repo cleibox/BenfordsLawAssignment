7f59cc52fda458aa6b81500b91160f48d6809de8
Sophia Nguyen <sophian3105@gmail.com>
2021-05-06 22:57:56 -0400

    Calculating frequencies for each number

--- BenfordsLawCode.java
+++ BenfordsLawCode.java
@@ -47,7 +47,7 @@ class BenfordsLawCode {
             }
             // User chooses an invalid option
             else{
-                System.out.println("Please type in a valid option (1 or 9)");
+                System.out.println("Please type in a valid option");
             }
         } while (!userInput.equals(exitCondition));      
         reader.close();
@@ -65,13 +65,24 @@ class BenfordsLawCode {
     // Reads the file
     public static void readFile(String route, String title){
         String line = "";
+        int frequency = 0;
+        int count = 0;
+        int[] frequencyArr = new int[9];
+        int[] tempArr = new int[9];
         try{ 
             File file = new File(route+title);
             BufferedReader br = new BufferedReader(new FileReader(file));
             while ((line = br.readLine()) != null){
                 // Calls the count value
-                countValue(line);
+                //count = countValue(line,frequency);
+                //frequency = count;
+                //System.out.println(frequency);
+                
+                // Populates the array
+                tempArr = countValue(line, frequencyArr);
+                frequencyArr = tempArr;
             }
+            printArray(frequencyArr);
         }
         // Program cannot find file
         catch (IOException e){ 
@@ -79,10 +90,54 @@ class BenfordsLawCode {
         }
     }
     // Takes the first digit
-    public static void countValue(String number){
+    public static int[] countValue(String number, int[] frequencyArr){
         String line  = number;
-        // Print out 1st frequency
         char firstNumber = line.charAt(4);
-        System.out.println(firstNumber);
+        // Checking frequency for 1
+        if(Character.compare(firstNumber, '1') == 0){
+            frequencyArr[0] += 1;
+        }
+        // Checking frequency for 2
+        else if(Character.compare(firstNumber, '2') == 0){
+            frequencyArr[1] += 1;
+        }
+        // Checking frequency for 3
+        else if(Character.compare(firstNumber, '3') == 0){
+            frequencyArr[2] += 1;
+        }
+        // Checking frequency for 4
+        else if(Character.compare(firstNumber, '4') == 0){
+            frequencyArr[3] += 1;
+        }
+        // Checking frequency for 5
+        else if(Character.compare(firstNumber, '5') == 0){
+            frequencyArr[4] += 1;
+        }
+        // Checking frequency for 6
+        else if(Character.compare(firstNumber, '6') == 0){
+            frequencyArr[5] += 1;
+        }
+        // Checking frequency for 7
+        else if(Character.compare(firstNumber, '7') == 0){
+            frequencyArr[6] += 1;
+        }
+        // Checking frequency for 8
+        else if(Character.compare(firstNumber, '8') == 0){
+            frequencyArr[7] += 1;
+        }
+        // Checking frequency for 9
+        else if(Character.compare(firstNumber, '9') == 0){
+            frequencyArr[8] += 1;
+        }
+        return frequencyArr;
+    
+    }
+    // Print the array
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]);
+            System.out.print("|");
+        }
+        System.out.println();
     }
 }
